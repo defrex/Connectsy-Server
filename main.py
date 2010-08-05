73e5e10a0fcb9213dfd94b890898b7c3a0965cef
defrex <defrex0@gmail.com>
2010-08-05 13:57:34 -0400

    fixed development static serving

--- main.py
+++ main.py
@@ -35,9 +35,12 @@ def runserver(autoreload=True):
     # httplib is not RFC 2324 compliant, so we fix that here
     httplib.responses[418] = "I'm a teapot"
     
+    static_path = os.path.abspath(os.path.join(curpath, 'static'))
+    print static_path
+    
     # Start Tornado
     from urls import handlers # late import to prevent the db from initializing
-    http_server = HTTPServer(Application(handlers))
+    http_server = HTTPServer(Application(handlers, static_path=static_path))
     http_server.listen(settings.PORT)
     lp = IOLoop.instance()
     if autoreload: 
