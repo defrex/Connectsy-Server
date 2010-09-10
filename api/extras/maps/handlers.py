292c758fbaafaa3db493d071031b3cf3829c53e4
defrex <defrex0@gmail.com>
2010-09-10 19:35:57 -0400

    added special map url

---
+++ api/extras/maps/handlers.py
@@ -0,0 +1,22 @@
+
+from base_handlers import BaseHandler
+import json
+
+class CategoriesHandler(BaseHandler):
+    
+    def get(self):
+        #grab the sorting type from the args
+        sort = self.get_argument('sort', None)
+        
+        #grab lat/lng from the query, defaulting to toronto
+        lat = float(self.get_argument('lat', '43.652527'))
+        lng = float(self.get_argument('lng', '-79.381961'))
+        events = db.objects.event.find({u'posted_from': {u'$near': [lat, lng]}})
+        events = events.sort(u'when', direction=DESCENDING)
+        resp = list()
+        for e in events:
+            resp.append({
+                    u'rev': e[u'revision'],
+                    u'loc': e[u'posted_from'],
+                })
+        self.output(resp)
