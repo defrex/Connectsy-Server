7826fc637bb76379e886a7676fca2e7e85dbcf83
Patrick Stein <lylepstein@gmail.com>
2010-09-21 03:01:24 -0400

    Fixed WIP code to actually run

--- api/events/handlers.py
+++ api/events/handlers.py
@@ -65,6 +65,7 @@ class EventsHandler(BaseHandler):
             #grab a list of unique events started by friends or with friends
             #invited, that are either open or have the current user invited
             #events = db.objects.
+            pass
         #any other value for filter is a category
         elif filter:
             events = events.where({u'category', filter})
