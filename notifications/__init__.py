d8ac8277d4267d62b9c9d1e94ffefd418b111158
Patrick Stein <lylepstein@gmail.com>
2010-09-24 15:10:28 -0400

    Hopefully fixed notifications

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -6,7 +6,7 @@ def send(user, message):
     '''
     Sends a message to all of a user's registered listeners
     '''
-    listeners = db.objects.notification.find({u'user': user})
+    listeners = db.objects.notification_reg.find({u'user': user})
     for listener in listeners:
         notifiers[listener[u'client_type']].send(user, listener[u'client_id'],
                 message)
