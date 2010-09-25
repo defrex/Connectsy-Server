6ce86810d6c6803417e0f8433e885842b1a49dcc
Patrick Stein <lylepstein@gmail.com>
2010-09-24 21:32:24 -0400

    Added basic console

---
+++ console.py
@@ -0,0 +1,26 @@
+'''
+Connectsy console
+'''
+
+import os
+import sys
+from code import InteractiveConsole
+
+#add lib to path
+curpath = os.path.dirname(__file__)
+sys.path.insert(0, os.path.abspath(os.path.join(curpath, 'lib')))
+
+if __name__ == '__main__':
+    print 'Connectsy REPL'
+    print
+    
+    globals = {}
+    locals = {}
+    
+    repl = InteractiveConsole()
+    while True:
+        #print '\n> ',
+        repl.push(repl.raw_input('>>> '))
+        #repl.push(sys.stdin.readline())
+            
+    print 'Bye'
