13b90626376872b213bff76456c52cded50ffea9
Patrick Stein <lylepstein@gmail.com>
2010-09-27 22:35:17 -0400

    Base api root nicer

--- api/handlers.py
+++ api/handlers.py
@@ -3,23 +3,36 @@ import time
 import settings
 from base_handlers import BaseHandler
 
+def nice_time(span):
+    '''
+    Generates a nice time description from a span
+    '''
+    if span > 60 * 60 * 24: #days
+        return u'%s days' % span / (60 * 60 * 24)
+    elif span > 60 * 60: #hours
+        return u'%s hours' % span / 360
+    elif span > 60: #minutes
+        return u'%s minutes' % span / 60
+    else:
+        return u'%s seconds' % span
+        
+
 start_time = time.time()
 class RootHandler(BaseHandler):
     def get(self):
-        version = u'0.2'
+        version = u'1.0'
         mode = u'Development' if settings.DEVELOPMENT else u'Production'
         uptime = time.time() - start_time
         
-        self.set_status(418)
-        self.set_header('X-Server', 'Connectsy-Teapot/0.1 Freshly Brewed')
-        self.set_header('Content-Type', 'text/plain; charset=UTF-8')
+        self.set_status(200) # As fun as 418 was, pingdom don't like it :(
+        self.set_header('Content-Type', 'text/plain; charset=utf-8')
         self.write(u'''
         Connectsy API Server
             Version: %s
             Mode:    %s
-            Uptime:  %s seconds
+            Uptime:  %s
             
         "Everything should be made as simple as possible, but not simpler."
           -- Albert Einstein
-        ''' % (version, mode, uptime))
+        ''' % (version, mode, nice_time(uptime)))
         self.finish()
