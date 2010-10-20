9659f83c213a6f4dd732efebb6c0d6038e9a510a
defrex <aron@connectsy.com>
2010-10-20 17:12:22 +0000

    gunicorn changes

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -11,11 +11,13 @@ workers = numCPUs() * 2 + 1
 bind = "127.0.0.1:8000"
 
 # see http://gunicorn.org/design.html
-worker_class = 'tornado'
+#worker_class = 'tornado'
 
-daemon = True
+#daemon = True
 
 logfile = '/var/log/connectsy-server.log'
 
 # process name in top, ps, etc
 proc_name = 'connectsy-server'
+
+user = 'www-data'
