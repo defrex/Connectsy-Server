3a29bb976461187711b43fc7c9151f4a91681cc0
defrex <defrex@gmail.com>
2010-11-08 14:34:05 -0500

    testing on server

--- urls.py
+++ urls.py
@@ -1,17 +1,16 @@
-from api.SMS.handlers import SMSHandler
-from api.categories.handlers import CategoriesHandler
-from api.events.attendance.handlers import AttendanceHandler
-from api.events.comments.handlers import CommentsHandler
+from api.handlers import RootHandler
+from api.tokens.handlers import TokenHandler
+from api.notifications.handlers import RegistrationHandler
+from api.notifications.generic_poll_handler import GenericPollHandler
+from api.users.handlers import UsersHandler, UserHandler, AvatarHandler
+from api.users.friends.handlers import FriendHandler, FriendsHandler
 from api.events.handlers import EventHandler, EventsHandler
 from api.events.invites.handlers import InvitesHandler
-from api.extras.email.handlers import BetaEmailHandler
+from api.events.attendance.handlers import AttendanceHandler
+from api.events.comments.handlers import CommentsHandler
+from api.categories.handlers import CategoriesHandler
 from api.extras.maps.handlers import MapsHandler
-from api.handlers import RootHandler
-from api.notifications.generic_poll_handler import GenericPollHandler
-from api.notifications.handlers import RegistrationHandler
-from api.tokens.handlers import TokenHandler
-from api.users.friends.handlers import FriendHandler, FriendsHandler
-from api.users.handlers import UsersHandler, UserHandler, AvatarHandler
+from api.SMS.handlers import SMSHandler
 
 handlers = [
     (r"/", RootHandler),
@@ -31,6 +30,6 @@ handlers = [
     (r"/categories/", CategoriesHandler),
     (r"/extras/maps/", MapsHandler),
     (r"/extras/SMS/", SMSHandler),
-    #(r"/extras/beta_email/", BetaEmailHandler),
+#    (r"/extras/beta_email/", BetaEmailHandler),
 ]
 
