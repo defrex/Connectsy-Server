b71716fc34fc6a53b711735445feb63dea28b46f
defrex <defrex@gmail.com>
2010-11-12 16:26:41 -0500

    testing on server

--- tests/main/events/invites.py
+++ tests/main/events/invites.py
@@ -27,3 +27,26 @@ class EventInvites(ConsyTestCase):
         self.assertNotEqual(att, None, 'user auto-invited')
         
     
+    def test_new_event_invite(self):
+        user = self.get_user()
+        user2 = self.make_user(username='user2')
+        
+        response = self.post('/users/%s/friends/' % user2[u'username'])
+        self.assertEqual(response.status, 200, 'friend request')
+        response = self.post('/users/%s/friends/' % user[u'username'],
+                             auth_user=user2)
+        self.assertEqual(response.status, 200, 'friend confirmation')
+        
+        response = self.post('/events/', {
+            u'broadcast': False,
+            u'what': 'Testin event creation',
+            u'users': [user2[u'username']],
+        })
+        self.assertEqual(response.status, 201, 'new event 201')
+        event = json.loads(response.read())
+        
+        att = Attendant.get({u'event': event[u'id'], u'user': user2[u'id']})
+        
+        self.assertNotEqual(att, None, 'user auto-invited')
+        
+    
