b2e7ac301dc51b0c009b9c89ce41be48e41ac1de
defrex <defrex@gmail.com>
2010-11-29 21:36:23 +0000

    new sms reg test

--- tests/main/SMS/registration.py
+++ tests/main/SMS/registration.py
@@ -33,3 +33,26 @@ class SMSRegistration(ConsyTestCase):
         self.assertNotEqual(sms_reg[0][u'twilio_number'], 
                             sms_reg[1][u'twilio_number'], 
                             'different twillio numbers')
+    
+    def test_sms_registration_over(self):
+        if not settings.TEST_SMS: return
+        
+        number = u'11111111111'
+        
+        for i in range(0, len(settings.TWILIO_NUMBERS)):
+            response = self.post('/events/', {
+                u'broadcast': True,
+                u'what': 'Testin',
+                u'timezone': 'Etc/UTC',
+                u'contacts': [{u'name': u't%i' % i, u'number': number}]
+            })
+            self.assertEqual(response.status, 201, 'new event 201')
+        
+        response = self.post('/events/', {
+            u'broadcast': True,
+            u'what': 'Testin',
+            u'timezone': 'Etc/UTC',
+            u'contacts': [{u'name': u'tx', u'number': number}]
+        })
+        self.assertEqual(response.status, 409, 'out of numbers')
+        
